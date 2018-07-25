00fa0242a2cfcfc8a789e0b6eefa5e40af4cfbce
root <root@DESKTOP-ATSKGNB.localdomain>
2018-07-25 19:06:54 +0800

    *****Linked list cycle

--- linked-list/linked-list-cycle/main.cpp
+++ linked-list/linked-list-cycle/main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 /*
 	The idea is to have two references to the list and move them difference speeds, move one by 1 node and the other by 2 node.
 	* If list have a loop they will definitely meet.
-	* Else either of them(ot their next) will be NULL.
+	* Else either of them(or their next) will be NULL.
 */
 
 struct ListNode {
@@ -26,11 +26,37 @@ struct ListNode {
 class Solution {
 public:
 	bool hasCycle(ListNode *head) {
+		ListNode * node_1 = head;
+		ListNode * node_2 = head;
 
+		while (node_1 != NULL && node_1->next != NULL && 
+			node_2 != NULL && node_2->next != NULL && node_2->next->next != NULL) {
+
+			node_1 = node_1->next;
+			node_2 = node_2->next->next;
+
+			if (node_1 == node_2) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 };
 
 int main()
 {
+	Solution so;
+
+	ListNode * l1 = new ListNode(1);
+	ListNode * l2 = new ListNode(2);
+	ListNode * l3 = new ListNode(3);
+
+	l1->next = l2;
+	l2->next = l3;
+	//l3->next = l1;
+
+	cout << so.hasCycle(l1) << endl;
+
 	return 0;
 }
