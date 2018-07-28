03a6f7606b9923adcc4568a31ca04e0d2b7dd289
yinchao <wsyinchao@outlook.com>
2018-07-28 09:44:42 +0800

    restudy dechotomy search

--- sorting-and-searching/first-bad-version/main.cpp
+++ sorting-and-searching/first-bad-version/main.cpp
@@ -3,34 +3,46 @@
 using namespace std;
 
 // Forward declaration of isBadVersion API.
-bool isBadVersion(int version);
+bool isBadVersion(int version) {
+	if (version == 1702766719) {
+		return true;
+	}
+
+	return false;
+}
 
 class Solution {
 public:
 	int firstBadVersion(int n) {
-		int ret = n; //The first bad version
-
-		int low = 0;
-		int high = n;
-
 		//dichotomy search.
-		int j = n / 2;
+		long long l = 1;
+		long long h = n;
 
-		while (j > 0 && j <= n) {
-			if (isBadVersion(j)) {
-				ret = j;
-				//j = (low + high) / 2
+		while (l < h) {
+			long long index = (l + h) / 2;
+			if (isBadVersion(index)) {
+				h = max(index - 1, l);
 			}
 			else {
-				j = j + (n - j + 1) / 2;
+				l = min(index + 1, h);
 			}
 		}
 
-		return ret;
+		if (l == h) {
+			if (isBadVersion(l)) {
+				return l;
+			}
+			else {
+				return l + 1; //This reason of "return l + 1" is 问题本身的性质决定.
+			}
+		}
 	}
 };
 
 int main()
 {
+	Solution so;
+	cout << so.firstBadVersion(2126753390) << endl;
+
 	return 0;
 }
