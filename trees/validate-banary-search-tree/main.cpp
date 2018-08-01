3ad9587b7bc6cb27e289aa4b8a67546a54052193
yinchao <wsyinchao@outlook.com>
2018-08-01 20:47:06 +0800

    clean workspace

---
+++ trees/validate-banary-search-tree/main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+
+//Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	bool isValidBST(TreeNode* root) {
+		//should resolve it with recursively.
+		
+		//check leaf nodes counts.
+
+		//...
+	}
+};
+
+int main()
+{
+
+
+	return 0;
+}
