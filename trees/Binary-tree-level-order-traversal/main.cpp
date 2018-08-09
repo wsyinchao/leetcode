679dd0d3564ed63e51af2d20f4359524aa7b07c9
yinchao <wsyinchao@outlook.com>
2018-08-09 08:53:46 +0800

    symmetric binary tree

---
+++ trees/Binary-tree-level-order-traversal/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+
+//Definition for a binary tree node.
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+class Solution {
+public:
+	vector<vector<int>> levelOrder(TreeNode* root) {
+
+	}
+};
+
+int main()
+{
+	return 0;
+}
--- trees/symmetric-tree/main.cpp
+++ trees/symmetric-tree/main.cpp
@@ -13,7 +13,20 @@ using namespace std;
 class Solution {
 public:
 	bool isSymmetric(TreeNode* root) {
-		
+		return subIsSymmetric(root, root);
+	}
+
+	bool subIsSymmetric(TreeNode * leftSubTree, TreeNode * rightSubTree) {
+		if (leftSubTree == NULL && rightSubTree == NULL) {
+			return true;
+		}
+		if (leftSubTree == NULL || rightSubTree == NULL) {
+			return false;
+		}
+
+		return leftSubTree->val == rightSubTree->val &&
+			subIsSymmetric(leftSubTree->left, rightSubTree->right) &&
+			subIsSymmetric(leftSubTree->right, rightSubTree->left);
 	}
 };
 
